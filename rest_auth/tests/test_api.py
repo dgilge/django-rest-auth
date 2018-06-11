cbf32ca1d1e10bd40245100d8c9a5f4cfc373c4e
Daniel <33256939+dgilge@users.noreply.github.com>
2018-06-11 08:53:51 +0200

    Fix test

--- rest_auth/tests/test_api.py
+++ rest_auth/tests/test_api.py
@@ -502,7 +502,7 @@ class APIBasicTests(TestsMixin, TestCase):
         get_user_model().objects.create_user(self.USERNAME, '', self.PASS)
 
         resp = self.post(self.login_url, data=payload, status_code=200)
-        self.assertEqual(resp.json(), {'detail': 'Successfully logged out.'})
+        self.assertEqual(resp.json, {'detail': 'Successfully logged out.'})
         self.get(self.logout_url, status=status.HTTP_200_OK)
 
     @override_settings(ACCOUNT_LOGOUT_ON_GET=False)
